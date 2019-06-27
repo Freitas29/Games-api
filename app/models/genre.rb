ca079a44b08eba6934b6a31eaabcaa648ba638b2
Freitas29 <arao.freitas@hotmail.com>
2019-06-27 02:01:04 -0300

    genre added

---
+++ app/models/genre.rb
@@ -0,0 +1,4 @@
+class Genre < ApplicationRecord
+  validates :description, presence: true
+  validates :description, length: {in: 1...50}
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_044716) do
+ActiveRecord::Schema.define(version: 2019_06_27_045748) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
