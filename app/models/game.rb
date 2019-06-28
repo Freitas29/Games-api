c2759402dd6ddc9057b7a8f37679b7870c894ef2
Freitas29 <arao.freitas@hotmail.com>
2019-06-27 23:29:53 -0300

    fixed validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
   validates :title, :description, presence: true
-  validates :title, length: { in: 5...50 }
-  validates :description, length: { in: 5...250 }
+  validates :title, length: { in: 5..50 }
+  validates :description, length: { in: 5..250 }
 
   has_and_belongs_to_many :genres
   has_and_belongs_to_many :plataforms
