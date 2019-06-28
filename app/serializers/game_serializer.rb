fd1de3e37342cfcea4a33c690538ed34a79b18cd
Freitas29 <arao.freitas@hotmail.com>
2019-06-27 23:40:44 -0300

    enhancement serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,5 +1,12 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :genres, :plataforms
 
+  def genres
+    object.genres.pluck(:description)
+  end
 
+  def plataforms
+    object.plataforms.pluck(:description)
+  end
+  
 end
