d77ce7fa17463b26f0b436a5acecdb87b154b5f2
Freitas29 <arao.freitas@hotmail.com>
2019-06-27 23:31:17 -0300

    one more ...

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ApplicationRecord
   validates :description, presence: true
-  validates :description, length: {in: 1...50}
+  validates :description, length: {in: 1..50}
 
   has_and_belongs_to_many :games
 end
